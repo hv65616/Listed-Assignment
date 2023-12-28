23286bd01deb968b03572b5db9730a2ffbfda3f2
Himanshu <hv65616@gmail.com>
2023-12-28 21:57:10 +0530

    initial commit

---
+++ credentials.js
@@ -0,0 +1,7 @@
+const CLIENT_ID =
+  "56233659966-asgvn21kvrjj0fbhvft303anv41hjpev.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04h1GcwmorWoiCgYIARAAGAQSNwF-L9IrdyLT3_Pe4M2S4CIJzwmDj0IHfRhw9Nm-k1F_OBmLvALHrRdO4VzmqHwA1_dogZv9YV8";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
